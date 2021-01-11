4d7b41995605be9f66d8ae15aa51a1395df29480
iangu48 <ian.gu@mail.utoronto.ca>
2021-01-10 22:41:58 -0500

    allow cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var uploadRouter = require('./routes/upload');
 var searchRouter = require('./routes/search');
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
